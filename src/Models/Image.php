2fa8af50b8e234a3713a2672b6075ed1b4bf4b93
Sidas Snieska <ssnieskiokas@gmail.com>
2019-07-01 13:58:20 +0300

    0.0.5

---
+++ src/Models/Image.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Prophe1\Image;
+
+use function App\Prophe1\Image\Utils\getFiletypeByLink;
+use function App\Prophe1\Image\Utils\getImageAltByID;
+use function App\Prophe1\Image\Utils\getImageUrlByID;
+use function App\Prophe1\Image\Utils\getTitleByID;
+
+final class Image
+{
+    /**
+     * Image data
+     *
+     * @since 0.0.5
+     *
+     * @var array
+     */
+    private $data;
+
+    /**
+     * Allowed image attributes
+     *
+     * @since 0.0.5
+     *
+     * @var array
+     */
+    private $allowedAttributes = [
+        'alt',
+        'title',
+    ];
+
+    /**
+     * Image constructor.
+     *
+     * @since 0.0.1
+     *
+     * @param int $id
+     * @param string $size
+     */
+    public function __construct( int $id, string $size )
+    {
+        $this->data = [
+            'id' => $id,
+            'src' => getImageUrlByID($this->getID(), $size),
+            'size' => $size,
+            'type' => getFiletypeByLink($this->getUrl()),
+            'alt' => getImageAltByID($this->getID()),
+            'title' => getTitleByID($this->getID()),
+        ];
+    }
+
+    /**
+     * Checks if image is SVG
+     *
+     * @since 0.0.1
+     *
+     * @return bool
+     */
+    public function isSvg(): bool
+    {
+        $filetype = $this->getFiletype();
+
+        if (isset($filetype['ext']) && $filetype['ext'] === 'svg') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Svg content
+     *
+     * @since 0.0.1
+     *
+     * @return string
+     */
+    public function svg(): string
+    {
+        return trim(file_get_contents(get_attached_file( $this->getID() )));
+    }
+
+    /**
+     * ID Getter
+     *
+     * @return int
+     */
+    public function getID(): int
+    {
+        return $this->data['id'];
+    }
+
+    /**
+     * Link Getter
+     *
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->data['url'];
+    }
+
+    /**
+     * Alt Getter
+     *
+     * @return string
+     */
+    public function getAlt(): string
+    {
+        return $this->data['alt'];
+    }
+
+    /**
+     * Default size getter
+     *
+     * @return string|null
+     */
+    public function getSize(): string
+    {
+        return $this->data['size'];
+    }
+
+    /**
+     * Title getter
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->data['title'];
+    }
+
+    /**
+     * Get filetype
+     *
+     * @return array
+     */
+    public function getFiletype(): array
+    {
+        return $this->data['type'];
+    }
+
+    /**
+     * Get attributes
+     *
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+        return array_intersect($this->data, $this->allowedAttributes);
+    }
+}
---
+++ src/Render.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Prophe1\Image;
+
+final class Render
+{
+    /**
+     * Get Image sourcing
+     *
+     * @since 0.0.1
+     *
+     * @param Image $image
+     * @param array $sizes
+     *
+     * @return string
+     */
+    private static function sources(Image $image, array $sizes): string
+    {
+        $sources = '';
+
+        foreach ($sizes as $size => $media) {
+            $url = Image::getImageUrlByID($image->getID(), $size);
+
+            if ($image->getSize() === $size) {
+                $url = $image->getUrl();
+            }
+
+            $sources .= sprintf('<source srcset="%1$s" media="%2$s">',
+                $url,
+                $media
+            );
+        }
+
+        return $sources;
+    }
+
+    /**
+     * Outputs image html with sources
+     *
+     * @since 0.0.1
+     *
+     * @param int $id
+     * @param string|null $default
+     * @param array $sizes
+     *
+     * @return string
+     */
+    public static function html(
+        int $id,
+        ?string $default = null,
+        array $sizes = [] ): string
+    {
+        $image = new Image( $id, $default );
+
+        if ($image->isSvg())
+        {
+            return $image->svg();
+        }
+
+        return sprintf('
+            <picture>
+                %2$s
+                <img srcset="%1$s" alt="%3$s" title="%4$s">
+            </picture>',
+            $image->getUrl(),
+            self::sources($image, $sizes),
+            $image->getAttributes()
+        );
+    }
+}
